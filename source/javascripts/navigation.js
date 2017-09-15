c70bf8f912a5db750f17b19956ad35a262eaab6f
jamgar <jegpub@gmail.com>
2017-09-15 07:02:33 -0500

    Add debounce effect to navigation bar

---
+++ source/javascripts/navigation.js
@@ -0,0 +1,28 @@
+// debounce function that will wrap our event
+function debounce(fn, delay = 20) {
+  // maintain a timer
+  let timer = null;
+  // closure function that has access to timer
+  return function() {
+    // get the scope and parameters of the function
+    // via 'this' and 'arguments'
+    let context = this;
+    let args = arguments;
+    // if event is called, clear the timer and start over
+    clearTimeout(timer);
+    timer = setTimeout(function() {
+      fn.apply(context, args);
+    }, delay);
+  }
+}
+
+function scrolled() {
+  var topNav = document.querySelector('#top-nav');
+  if(window.scrollY !== 0) {
+    topNav.classList.remove("top-nav-home");
+  } else {
+    topNav.classList.add("top-nav-home");
+  }
+}
+
+window.addEventListener('scroll', debounce(scrolled));
